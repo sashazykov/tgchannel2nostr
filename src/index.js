de58d44f935a068ef1ce70b20e5029bf267b37ce
Aleksandr Zykov <alexandrz@gmail.com>
2026-01-06 11:05:58 +0100

    Remove initial comments and welcome message from index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { generateNip01Event, sendEvent } from './nostr';
 
 const MEDIA_GROUP_FLUSH_MS = 2000;
